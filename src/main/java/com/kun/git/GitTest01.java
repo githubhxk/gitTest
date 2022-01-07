93409264d05b676ff09cd330277ce7d6210bfe4b
hxk <hxk@163.com>
2022-01-07 15:13:53 +0800

    second commit

--- src/main/java/com/kun/git/GitTest01.java
+++ src/main/java/com/kun/git/GitTest01.java
@@ -3,5 +3,6 @@ package com.kun.git;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("helloGit....");
+        System.out.println("2helloGit....");
     }
 }
